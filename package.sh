5237daaf815cacf073af1a22c558ebda1f36d806
flatsiedatsie <unfold@gmail.com>
2020-11-14 10:31:19 +0100

    0.0.5

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
 
 # Clean up from previous releases
@@ -16,14 +16,15 @@ fi
 
 
 # Prep new package
-mkdir lib package
+mkdir package
 
 # Pull down Python dependencies
 #pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
 # Put package together
-cp -r lib pkg shairport rpiplay LICENSE manifest.json package.json *.py README.md package/
+cp -r pkg shairport rpiplay LICENSE manifest.json *.py README.md package/
 find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
 find package -type d -empty -delete
 
 # Generate checksums
@@ -34,9 +35,10 @@ cd -
 
 # Make the tarball
 echo "creating archive"
-TARFILE="airport-${version}${TARFILE_SUFFIX}.tgz"
+TARFILE="airport-${version}.tgz"
 tar czf ${TARFILE} package
 
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
-sha256sum ${TARFILE}
+#sha256sum ${TARFILE}
+cat ${TARFILE}.sha256sum
 #rm -rf SHA256SUMS package
