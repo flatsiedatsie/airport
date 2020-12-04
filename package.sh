eeef061ca056ebc4e86bf4020f3aaa488fecedae
flatsiedatsie <unfold@gmail.com>
2020-12-04 14:53:23 +0100

    0.1.0

---
+++ package.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+
+
+# Clean up from previous releases
+rm -rf *.tgz package SHA256SUMS lib *.sha256sum
+
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
+
+
+
+# Prep new package
+mkdir package
+
+# Pull down Python dependencies
+#pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
+
+# Put package together
+cp -r pkg shairport rpiplay LICENSE manifest.json *.py README.md package/
+find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
+find package -type f -name '.sha256sum' -delete 
+find package -type d -empty -delete
+
+# Generate checksums
+echo "generating checksums"
+cd package
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+cd -
+
+# Make the tarball
+echo "creating archive"
+TARFILE="airport-${version}.tgz"
+tar czf ${TARFILE} package
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+cat ${TARFILE}.sha256sum
+
+#rm -rf SHA256SUMS package
