6bc564591054140f9c1207e657d74ad5d96a767f
flatsiedatsie <unfold@gmail.com>
2023-01-25 10:21:25 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -28,7 +28,7 @@ pip3 install -r requirements.txt -t lib --no-cache --no-binary :all: --no-cache 
 
 # Put package together
 #cp -r lib pkg LICENSE manifest.json *.py README.md  package/
-cp -r pkg shairport rpiplay LICENSE *.json *.py package/
+cp -r pkg shairport shairport64 rpiplay LICENSE *.json *.py package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
 find package -type d -empty -delete
